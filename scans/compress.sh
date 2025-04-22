85b8acaa4b3bb36044b7cdf6e066ba82cefc8309
Efe Enobakhare <39485724+jadegamesuk@users.noreply.github.com>
2025-04-22 15:15:30 +0100

    Add files via upload

---
+++ scans/compress.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+OIFS="$IFS"
+IFS=$'\n'
+for INPUTPDF in `find * -type f -name "*.pdf"`  
+do
+    echo "Processing file: $INPUTPDF"
+    OUTPUTPDF=$(echo $INPUTPDF | cut -sd '/' -f2)    
+
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -dEmbedAllFonts=true -dSubsetFonts=true -dColorImageResolution=150 -dColorImageResolution=150 -dGrayImageResolution=150 -sOutputFile=output/$OUTPUTPDF $INPUTPDF
+
+    echo -e "Completed processing file: output/$OUTPUTPDF \n"
+
+done
+IFS="$OIFS"
